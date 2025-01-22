b04891936a932fbfaa626304387f209f9edf6f7e
DifaaNug <123245678+DifaaNug@users.noreply.github.com>
2025-01-23 01:13:06 +0700

    feat: add Poppins font styling to global CSS and layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,13 @@
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { Poppins } from "next/font/google";
 
+const poppins = Poppins({
+  subsets: ["latin"], // Pilih subset sesuai kebutuhan
+  weight: ["400", "500", "600", "700"], // Tambahkan berat font yang diinginkan
+  variable: "--font-poppins", // Variabel CSS untuk font
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -10,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.variable}>
       <body>
         <Navbar />
         {children}
